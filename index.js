f4859b6c39c6ece8f3ff188ff10504585f591db7
Steamgx <your-bmuraya646@gmail.com>
2024-11-27 15:38:08 +0300

    full

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 
-function receivesAFunction(callback) {
-    callback()
-  }
-  function returnsANamedFunction() {
-    return function named() {
-      console.log("The girl is Arya.")
-    }
-  }
-  function returnsAnAnonymousFunction() {
-    return function() {
-      console.log('The girl has no name.')
-    }
-  }
-  console.log('Starting execution of receivesAFunction');
-  receivesAFunction(function() {
-    console.log('Callback executed');
-  });
-  console.log('Execution of receivesAFunction completed');
-  
-  console.log('Starting execution of returnsANamedFunction');
-  var namedFunction = returnsANamedFunction();
-  namedFunction();
-  console.log('Execution of returnsANamedFunction completed');
-  
-  console.log('Starting execution of returnsAnAnonymousFunction');
-  var anonymousFunction = returnsAnAnonymousFunction();
-  anonymousFunction();
-  console.log('Execution of returnsAnAnonymousFunction completed');
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
+};
